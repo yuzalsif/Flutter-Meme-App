8938c49238e37ca5fa8370fd182c2b5440387eb9
yuzalsif <liteit68@gmail.com>
2023-08-03 14:28:14 +0300

    Add QuoteDetailState

--- 03-managing-state-with-cubits-and-the-bloc-library/projects/starter/packages/features/quote_details/lib/src/quote_details_state.dart
+++ 03-managing-state-with-cubits-and-the-bloc-library/projects/starter/packages/features/quote_details/lib/src/quote_details_state.dart
@@ -1,3 +1,35 @@
 part of 'quote_details_cubit.dart';
 
-// TODO: Create models for all possible screen states.
+abstract class QuoteDetailsState extends Equatable {
+  const QuoteDetailsState();
+}
+
+class QuoteDetailsInProgess extends QuoteDetailsState {
+  const QuoteDetailsInProgess();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class QuoteDetailsSuccess extends QuoteDetailsState {
+  const QuoteDetailsSuccess({
+    required this.quote,
+    // TODO: Recieve new property
+  });
+
+  final Quote quote;
+  // TODO: Add new property
+
+  @override
+  List<Object?> get props => [
+        quote,
+        // TODO: List new property 
+      ];
+}
+
+class QuoteDetailsFailure extends QuoteDetailsState {
+  const QuoteDetailsFailure();
+
+  @override
+  List<Object?> get props => [];
+}
