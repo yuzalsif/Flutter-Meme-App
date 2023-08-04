6b6a49051ea335cbbb32a5509199d800b09af4d2
yuzalsif <liteit68@gmail.com>
2023-08-04 17:22:53 +0300

    feat: Add SignInState implementation

--- 04-validating-forms-with-cubits/projects/starter/packages/features/sign_in/lib/src/sign_in_state.dart
+++ 04-validating-forms-with-cubits/projects/starter/packages/features/sign_in/lib/src/sign_in_state.dart
@@ -1,6 +1,31 @@
 part of 'sign_in_cubit.dart';
 
-// TODO: Create your state class.
+class SignInState extends Equatable {
+  const SignInState({
+    this.email = const Email.unvalidated(),
+    this.password = const Password.unvalidated(),
+    this.submissionStatus,
+  });
+
+  final Email email;
+  final Password password;
+  final SubmissionStatus? submissionStatus;
+
+  SignInState copyWith(
+      {Email? email, Password? password, SubmissionStatus? submissionStatus}) {
+    return SignInState(
+        email: email ?? this.email,
+        password: password ?? this.password,
+        submissionStatus: submissionStatus);
+  }
+
+  @override
+  List<Object?> get props => [
+        email,
+        password,
+        submissionStatus,
+      ];
+}
 
 enum SubmissionStatus {
   /// Used when the form has not been sent yet.
